27b4514086bdec526b8d407a2de3f8b1d34c8f80
queen <queenscriptqueen@gmail.com>
2023-01-06 17:43:13 -0800

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    title: `Atlas`,
+    description: `Earn rewards on rent`,
+    image: `./src/svg/iconmark.png`,
+    siteUrl: `https://www.useatlas.co`,
+  },
   pathPrefix: "/landingpage",
   plugins: [
     {
